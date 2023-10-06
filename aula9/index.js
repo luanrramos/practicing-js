2d6fb31b7f9f7029afa408a9571888cd60b3300c
Luan Ramos <104947687+luanrramos@users.noreply.github.com>
2023-10-06 19:25:01 -0300

    adding

---
+++ aula9/index.js
@@ -0,0 +1,11 @@
+const nome = "Luan";
+const nome1 = 'Luan';
+const nome2 = `Luan`;
+const num1 = 10; //Number
+const num2 = 10.52; //Number
+let nomeAluno; //
+const sobrenomeAluno = null; //Nulo -> Não aponta pra locla nenhuma na memória
+
+typeof(console.log(sobrenomeAluno));
+console.log(typeof sobrenomeAluno)
+const boolean = true; //true ou false
