968f02a5b4781d3c05688c046a931600bd1f569b
Luan Ricardo <luanrnr1227@gmail.com>
2024-04-29 19:33:38 -0300

    Update - Functions (call, apply and bind)

--- Objects 2/Array/Loops/exercises.js
+++ Objects 2/Array/Loops/exercises.js
@@ -4,10 +4,9 @@
 
 const cursos = document.querySelectorAll(".curso");
 const cursosArray = Array.from(cursos);
-
-const objetoCursos = cursosArray.map((curso) => {
-  const titulo = curso.querySelector("h1").innerText;
-  const descricao = curso.querySelector("p").innerText;
+const cursosObj = cursosArray.map((curso) => {
+  const titulo = curso.querySelector(".curso h1").innerText;
+  const descricao = curso.querySelector(".curso p").innerText;
   const aulas = curso.querySelector(".aulas").innerText;
   const horas = curso.querySelector(".horas").innerText;
   return {
@@ -17,52 +16,68 @@ const objetoCursos = cursosArray.map((curso) => {
     horas: horas,
   };
 });
-// Retorne uma lista com os
-// números maiores que 100
-const numeros = [3, 44, 333, 23, 122, 322, 33];
-const numerosMaior100 = numeros.filter((numero) => {
-  return numero > 100;
-});
-console.log(numerosMaior100);
+
+// const cursos = document.querySelectorAll(".curso");
+// const cursosArray = Array.from(cursos);
+
+// const objetoCursos = cursosArray.map((curso) => {
+//   const titulo = curso.querySelector("h1").innerText;
+//   const descricao = curso.querySelector("p").innerText;
+//   const aulas = curso.querySelector(".aulas").innerText;
+//   const horas = curso.querySelector(".horas").innerText;
+//   return {
+//     titulo: titulo,
+//     descricao: descricao,
+//     aulas: aulas,
+//     horas: horas,
+//   };
+// });
+// // Retorne uma lista com os
+// // números maiores que 100
+// const numeros = [3, 44, 333, 23, 122, 322, 33];
+// const numerosMaior100 = numeros.filter((numero) => {
+//   return numero > 100;
+// });
+// console.log(numerosMaior100);
 
 // Verifique se Baixo faz parte
 // da lista de instrumentos e retorne true
-const instrumentos = ["Guitarra", "Baixo", "Bateria", "Teclado"];
-const instrumentoBaixo = instrumentos.some(
-  (instrumento) => instrumento === "Baixo"
-);
-console.log(instrumentoBaixo);
+// const instrumentos = ["Guitarra", "Baixo", "Bateria", "Teclado"];
+// const instrumentoBaixo = instrumentos.some(
+//   (instrumento) => instrumento === "Baixo"
+// );
+// console.log(instrumentoBaixo);
 
-// Retorne o valor total das compras
-const compras = [
-  {
-    item: "Banana",
-    preco: "R$ 4,99",
-  },
-  {
-    item: "Ovo",
-    preco: "R$ 2,99",
-  },
-  {
-    item: "Carne",
-    preco: "R$ 25,49",
-  },
-  {
-    item: "Refrigerante",
-    preco: "R$ 5,35",
-  },
-  {
-    item: "Quejo",
-    preco: "R$ 10,60",
-  },
-];
+// // Retorne o valor total das compras
+// const compras = [
+//   {
+//     item: "Banana",
+//     preco: "R$ 4,99",
+//   },
+//   {
+//     item: "Ovo",
+//     preco: "R$ 2,99",
+//   },
+//   {
+//     item: "Carne",
+//     preco: "R$ 25,49",
+//   },
+//   {
+//     item: "Refrigerante",
+//     preco: "R$ 5,35",
+//   },
+//   {
+//     item: "Quejo",
+//     preco: "R$ 10,60",
+//   },
+// ];
 
-let cont = 0;
-const total = compras.reduce((acc, produto, index) => {
-  const x = produto.preco.replace("R$ ", "");
-  const y = parseFloat(x);
+// let cont = 0;
+// const total = compras.reduce((acc, produto, index) => {
+//   const x = produto.preco.replace("R$ ", "");
+//   const y = parseFloat(x);
 
-  cont = parseFloat(cont + y);
-  return cont.toFixed(2);
-}, 0);
-console.log(total);
+//   cont = parseFloat(cont + y);
+//   return cont.toFixed(2);
+// }, 0);
+// console.log(total);
---
+++ Objects 2/Function/call()/script.js
@@ -0,0 +1,68 @@
+// function.call(this, arg1, arg2, ...) executa a função, sendo possível passarmos uma nova referência ao this da mesma.
+
+// const carro = {
+//   marca: "Ford",
+//   ano: 2018,
+// };
+
+// function descricaoCarro() {
+//   console.log(this);
+// }
+
+// descricaoCarro(); // undefined undefined
+// descricaoCarro.call({ marca: "Ferrari" }); // undefined undefined
+// descricaoCarro.call(carro); // Ford 2018
+
+// O valor de this faz referência ao objeto criado durante a construção do objeto (Constructor Function). Podemos trocar a referência do método ao this, utilizando o call().
+
+// const carros = ['Ford', 'Fiat', 'VW'];
+
+// carros.forEach((item) => {
+//   console.log(item);
+// }); // Log de cada Carro
+
+// carros.forEach.call(carros, (item) => {
+//   console.log(item);
+// }); // Log de cada Carro
+
+// const frutas = ['Banana', 'Pêra', 'Uva'];
+
+// carros.forEach.call(frutas, (item) => {
+//   console.log(item);
+// }); // Log de cada Fruta
+
+// EXEMPLO REAL
+// O objeto atribuído a lista será o substituído pelo primeiro argumento de call()
+
+function Dom(seletor) {
+  this.element = document.querySelector(seletor);
+  this.ativo = function (classe) {
+    this.element.classList.add(classe);
+  };
+}
+
+// Dom.prototype.ativo = function (classe) {
+//   this.element.classList.add(classe);
+// };
+
+// const lista = new Dom("ul");
+// lista.ativo("ativar");
+// console.log(lista);
+
+// Example 01 - call
+
+const jogo = {
+  nome: "GTA",
+  ano: 2024,
+};
+
+const carro = {
+  nome: "Toyota",
+  ano: 2000,
+
+  trocarNome: function (nome) {
+    return (this.nome = nome);
+  },
+};
+
+const trocarNomeJogo = carro.trocarNome.bind(jogo, "Teste");
