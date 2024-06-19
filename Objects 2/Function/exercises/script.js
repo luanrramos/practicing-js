f7c56ee66589f5c5fd36189fdf0417d45a035f87
Luan Ricardo <luanrnr1227@gmail.com>
2024-06-19 15:15:49 -0300

    Update - solved exercises

--- Objects 2/Function/exercises/script.js
+++ Objects 2/Function/exercises/script.js
@@ -90,13 +90,18 @@ function mediaFilhos() {
 }
 
 function maiorSalario() {
-  for (let i = 0; i = data.length; i++) {
-    console.log(data[i].salario);
+  let acc = 0;
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].salario > acc) {
+      acc = data[i].salario;
+    }
   }
+  return acc;
 }
-maiorSalario();
+
 // console.log(`A média salarial da população é de: ${mediaComFor()} R$`);
 // console.log(`A média de filhos da população é de: ${mediaFilhos()}`);
+// console.log(`O maior salário da população é de: ${maiorSalario()} R$`);
 
 // Com forEach
 function mediaComFE() {
@@ -123,3 +128,101 @@ function mediaComMap() {
   return media;
 }
 //  console.log(`A média salarial da população é de: ${mediaComMap()} R$`);
+
+function taxaMensalJ(vi, va, t) {
+  let calculoTaxa = ((va - vi) / vi) * t;
+  return `${calculoTaxa}%`;
+}
+
+// console.log(taxaMensalJ(1000, 1500, 13))
+
+// [ ] Escreva um programa onde, você cria uma função geradora de desconto.
+// A função aceita 4 parâmetros (Nome do cliente, valor total da compra, um booleano que diz se é a primeira compra do cliente ou não, um booleando que diz se o cliente vai pagar a vista ou não)
+// Os descontos funcionam da seguinte forma:
+// Se é a primeira compra do cliente, pagar a vista, e a compra for maior que R$ 1000 => 30% de desconto
+// Se é a primeira compra do cliente, pagar a vista, e a compra for menor que R$ 1000, e maior que R$ 500 => 25% de desconto
+// Se é a primeira compra do cliente, pagar a vista e a compra for menor que R$ 500 => desconto de 20%
+// Se é a primeira compra do cliente, NÃO pagar a vista, e a compra for maior que R$ 1000 => 20% de desconto -
+// Se é a primeira compra do cliente, NÃO pagar a vista, e a compra for menor que R$ 1000, e maior que R$ 500 => 15% de desconto
+// Se é a primeira compra do cliente, NÃO pagar a vista e a compra for menor que R$ 500 => desconto de 10%
+// Se NÃO é a primeira compra do cliente, pagar a vista, e a compra for maior que R$ 1000 => 20% de desconto
+// Se NÃO é a primeira compra do cliente, pagar a vista, e a compra for menor que R$ 1000, e maior que R$ 500 => 15% de desconto
+// Se NÃO é a primeira compra do cliente, pagar a vista e a compra for menor que R$ 500 => desconto de 10%
+// Se NÃO é a primeira compra do cliente, NÃO pagar a vista, e a compra for maior que R$ 1000 => 10% de desconto - Se NÃO é a primeira compra do cliente, NÃO pagar a vista, e a compra for menor que R$ 1000, e maior que R$ 500 => 5% de desconto
+// Se NÃO é a primeira compra do cliente, NÃO pagar a vista e a compra for menor que R$ 500 => SEM DESCONTO- A função deve imprimir na tela:
+// Se o cliente recebeu desconto, imprima na tela um agradecimento pela compra, o valor total sem desconto, o valor total com desconto e a porcentagem de desconto que ele recebeu.
+// Se o cliente não recebeu desconto, imprima na tela um agradecimento pela compra, o valor total da sua compra e um cupom de desconto. Esse cupom deve ser um valor aleatório entre 10 e 20% para a próxima compra.
+
+function geradorDesc(nomeCliente, valorTot, primeiroCompra, aVista) {
+  let desconto = 0;
+
+  //Condição para caso for a primeira compra da cliente e o pagamento for a vista
+  if (primeiroCompra && aVista) {
+    if (valorTot > 1000) {
+      desconto = valorTot * 0.3;
+      valorTot -= desconto;
+    } else if (valorTot > 500) {
+      desconto = valorTot * 0.25;
+      valorTot -= desconto;
+    } else if (valorTot < 500) {
+      desconto = valorTot * 0.2;
+      valorTot -= desconto;
+    }
+  }
+  //Condição para caso for a primeira compra da cliente e o pagamento NÃO for a vista
+  else if (primeiroCompra && !aVista) {
+    if (valorTot > 1000) {
+      desconto = valorTot * 0.2;
+      valorTot -= desconto;
+    } else if (valorTot > 500) {
+      desconto = valorTot * 0.15;
+      valorTot -= desconto;
+    } else if (valorTot < 500) {
+      desconto = valorTot * 0.1;
+      valorTot -= desconto;
+    }
+  }
+
+  //Condição para caso NÃO for a primeira compra da cliente e o pagamento for a vista
+  else if (!primeiroCompra && aVista) {
+    if (valorTot > 1000) {
+      desconto = valorTot * 0.2;
+      valorTot -= desconto;
+    } else if (valorTot > 500) {
+      desconto = valorTot * 0.15;
+      valorTot -= desconto;
+    } else if (valorTot < 500) {
+      desconto = valorTot * 0.1;
+      valorTot -= desconto;
+    }
+  }
+
+  //Condição para caso NÃO for a primeira compra da cliente e o pagamento NÃO for a vista
+  else if (!primeiroCompra && !aVista) {
+    if (valorTot > 1000) {
+      desconto = valorTot * 0.1;
+      valorTot -= desconto;
+    } else if (valorTot > 500) {
+      desconto = valorTot * 0.05;
+      valorTot -= desconto;
+    } else if (valorTot < 500) {
+      valorTot = valorTot;
+    }
+  }
+  if (desconto != 0) {
+    console.log(`Muito obrigado por comprar com a gente,${nomeCliente}!
+      Valor total sem desconto: ${valorTot + desconto} R$
+      Valor total com desconto: ${valorTot}`);
+  } else {
+    console.log(`Muito obrigado por comprar com a gente,${nomeCliente}!
+      Valor total da compra: ${valorTot} R$
+      Parabéns! Você ganhou um cupom de ${valorSorteado()}% de desconto no sorteio! `);
+
+  }
+}
+function valorSorteado(){
+  const numeroSorteado = Math.floor(Math.random() * 11) + 10
+
+  return numeroSorteado;
+}
+geradorDesc("Luana", 200, false, false);
